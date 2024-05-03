8fcf3500f7f726d0b362e2db0d230c68f5a32f2d
sittimunirahar <sittimunirahar@gmail.com>
2024-05-03 12:43:47 +0800

    fix: remove test query

--- src/main.jsx
+++ src/main.jsx
@@ -1,27 +1,12 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client'
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
 
 const client = new ApolloClient({
   uri: 'http://localhost:4000',
   cache: new InMemoryCache()
 })
-
-client.query({
-  query: gql`
-    query getStudents {
-      students {
-        id
-        name
-        college {
-          id
-          name
-        }
-      }
-    }`
-}).then((result) => console.log(result))
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <ApolloProvider client={client}>
